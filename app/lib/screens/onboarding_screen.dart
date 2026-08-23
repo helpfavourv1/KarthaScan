5539fef213028b68b10e4c47fc831d15a37b3fbe
helpfavourv1 <helpfavourv1@users.noreply.github.com>
2026-08-23 16:25:19 +0100

    Replace phosphor_flutter with phosphoricons_flutter (Dart 3.x compatible)

--- app/lib/screens/onboarding_screen.dart
+++ app/lib/screens/onboarding_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:phosphor_flutter/phosphor_flutter.dart';
+import 'package:phosphoricons_flutter/phosphoricons_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import '../core/utils/constants.dart';
